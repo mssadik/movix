c9a20ecb18f2aa674a0ecd3ca34edd1fb3821995
Sadik <anarul6777@gmail.com>
2023-09-01 11:57:01 +0600

    a small update on footer

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,9 +1,16 @@
-import  './style.scss'
+import React from "react";
+
+import "./style.scss";
+import ContentWrapper from "../../components/ContentWrapper/ContentWrapper";
+
 
 const PageNotFound = () => {
     return (
-        <div>
-            
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
         </div>
     );
 };
